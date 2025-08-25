eb668830b1b921a67b2928a54e2aa11ee988eb41
r3b1e <keptgamer29@gmail.com>
2025-08-25 10:46:26 +0530

    java program

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Simple {
     public static void main(String[] args) {
-        System.out.println("Sunny Gupta");
+        System.out.println("Sunny Gupta at apsit");
     }
 }
